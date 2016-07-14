a65ae31b00c4c8234e21020c345c9039fed627a5
Adam Juszt <juszta@gmail.com>
2016-07-14 11:27:07 +0200

    adding main page and minor modifications

---
+++ src/MainPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MainPage.Startup))]
+namespace MainPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
